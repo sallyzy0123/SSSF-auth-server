8c5ed7c5fcee314cf9b94259fc5d44cb2aa2006c
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2023-04-03 01:57:49 +0300

    first commit

---
+++ src/api/controllers/userController.ts
@@ -0,0 +1,8 @@
+// Description: This file contains the functions for the user routes
+// TODO: add function check, to check if the server is alive
+// TODO: add function to get all users
+// TODO: add function to get a user by id
+// TODO: add function to create a user
+// TODO: add function to update a user
+// TODO: add function to delete a user
+// TODO: add function to check if a token is valid
